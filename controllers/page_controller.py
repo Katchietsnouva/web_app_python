95430c75c640788eeaf78feafc740763f52a57da
Katchietsnouva <philipaswa01@gmail.com>
2024-02-06 01:07:16 +0300

    web update form  desktop 5.4

--- controllers/page_controller.py
+++ controllers/page_controller.py
@@ -159,6 +159,7 @@ class PageController:
                 flash('Booking Successful!', 'success')
 
                 # return redirect(url_for('success', message='Booking Successful!', duration=duration, redirect_url=url_for('home')))
+                return render_template('booking_page.html', duration=duration)
                 return render_template('booking_page.html', duration=duration, booking=time_model.to_dict())
             return render_template('booking_page.html')
 
