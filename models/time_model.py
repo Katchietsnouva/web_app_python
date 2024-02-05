4396262bb8ab575d775467d9b6aa8139382adcb9
Katchietsnouva <philipaswa01@gmail.com>
2024-02-05 22:52:07 +0300

    web update form  desktop 4.9

--- models/time_model.py
+++ models/time_model.py
@@ -1,4 +1,5 @@
 # models/time_model.py
+from datetime import datetime;
 class TimeModel:
     def __init__(self, user_id, customer_no, booking_id, arrival_date, arrival_time, departure_date, departure_time):
         self.user_id = user_id
@@ -8,3 +9,10 @@ class TimeModel:
         self.arrival_time = arrival_time
         self.departure_date = departure_date
         self.departure_time = departure_time
+
+    def calculate_duration(self):
+            arrival_datetime = datetime.strptime(f"{self.arrival_date} {self.arrival_time}", "%Y-%m-%d %H:%M")
+            departure_datetime = datetime.strptime(f"{self.departure_date} {self.departure_time}", "%Y-%m-%d %H:%M")
+
+            duration = departure_datetime - arrival_datetime
+            return duration
