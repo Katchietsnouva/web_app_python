4af22c1c50711eafafa4ff23ec3ff3ae3a91283a
Katchietsnouva <philipaswa01@gmail.com>
2024-02-05 13:08:59 +0300

    web update form  desktop 2.4

---
+++ models/time_model.py
@@ -0,0 +1,7 @@
+# models/time_model.py
+class TimeModel:
+    def __init__(self, arrival_date, arrival_time, departure_date, departure_time):
+        self.arrival_date = arrival_date
+        self.arrival_time = arrival_time
+        self.departure_date = departure_date
+        self.departure_time = departure_time
