3afa4156f94a72405dd0d629a40c5d32f3f0a114
Katchietsnouva <philipaswa01@gmail.com>
2024-02-16 12:27:55 +0300

    commitadminn

--- fire.py
+++ fire.py
@@ -5,10 +5,19 @@ from firebase_admin import db
 cred = credentials.Certificate('fig.json')
 
 
-firebase_admin.initialize_app(cred,{}:'')
+firebase_admin.initialize_app(cred,{'databaseURL':'https://plan1-e1ebc-default-rtdb.europe-west1.firebasedatabase.app/'})
 
-ref = db.reference('py/')
+ref = db.reference('parking_web_app_db/')
 user_ref = ref.child('users')
 user_ref.set({
-    ''
+    '1st user ':{
+        'name': 'a',
+        'number':11,
+        'id': 'a1'
+    },
+    '2nd user':{
+        'name': 'b',
+        'number':31,
+        'id': 'a5'
+    }
 })
