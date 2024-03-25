47bf7ed0cd8969dbc662e8bf009451199db224ba
nouva neaons linux developer <philipaswa01@gmail.com>
2024-03-25 16:04:10 +0300

    commit nouvaneons 0.088

--- controllers/page_controller.py
+++ controllers/page_controller.py
@@ -46,19 +46,24 @@ class PageController:
         
         @app.route('/slot_management',methods=['GET', 'POST'])
         def slot_management():
-            # if request.method == 'POST':
-            #     parking_slot_id = request.form.get('parking_slot_id')
-            #     slot_status = request.form.get('slot_status')
-            #     available_for_use = request.form.get('available_for_use')      
-                
-                parking_slot_id = UserController.generate_parking_slot_id
+            if request.method == 'POST':
+                # parking_slot_id = UserController.generate_parking_slot_id?
+                parking_slot_id = request.form.get('parking_slot_id')
                 slot_status = request.form.get('slot_status')
                 available_for_use = request.form.get('available_for_use')      
+                
                 # Create parking_slots_available_data instance
                 parking_slots_available_data_model = ParkingSlotAvailableModel(parking_slot_id, slot_status, available_for_use)
                 # Register the slot
-                add_parking_slot_successful = UserController.save_admin_added_parking_slots_available_data(self.user_controller, parking_slots_available_data_model)                    
-            return render_template('slot_management.html')        
+                add_parking_slot_successful = UserController.save_admin_added_parking_slots_available_data(self.user_controller, parking_slots_available_data_model)  
+                if add_parking_slot_successful:
+                    flash('Slot added successfully!', 'success')
+                else:
+                    flash('Failed to add slot.', 'error')                   
+            # Load existing slot data for display
+            all_parking_slots = UserController.get_all_parking_slots_available_data(self.user_controller)
+               
+            return render_template('slot_management.html',  all_parking_slots=all_parking_slots)  
         
 
         @app.route('/admin', methods=['GET'])
